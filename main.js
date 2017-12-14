32eb870de9ccbfd1b1639889cd6ebae71dadbfca
Rafael Bernardo <rafamaxber@gmail.com>
2017-12-13 22:19:16 -0200

    add get allCharacters

--- main.js
+++ main.js
@@ -1,46 +1,64 @@
 // example.js
 const express = require('express');
 const {
-  buildSchema,
-  GraphQLObjectType
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema
 } = require('graphql');
 const graphqlHTTP = require('express-graphql');
 
-const crypto = require('crypto');
-const axios = require('axios');
+//=====================
+
+  const crypto = require('crypto');
+  const axios = require('axios');
+
+  console.log('================');
+  const timestamp = Date.now();
+  const privateKey = '147882a8ecfb00e8d1e9d488a9fab40d6bd256f6';
+  const publicKey = '0d6cfd363cc74d7f35a76e5cb7eedefb';
+  const data = timestamp + privateKey + publicKey;
+  const hash = crypto.createHash('md5').update(data).digest("hex");
+
+  const api = `http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
+
+//=====================
 
 const port = 4000;
 
-//=====================
+const CharactersType = new GraphQLObjectType({
+  name: 'Characters',
+  description: 'All characters 0 a 20',
+  fields: () => ({
+    id: { 
+      type: GraphQLString,
+      resolve: character => character.id
+    },
+    name: { 
+      type: GraphQLString,
+      resolve: character => character.name
+    },
+    thumb: { 
+      type: GraphQLString,
+      resolve: character => `${character.thumbnail.path}.${character.thumbnail.extension}`
+    },
+  })
+});
 
-
-console.log('================');
-const timestamp = Date.now();
-const privateKey = '147882a8ecfb00e8d1e9d488a9fab40d6bd256f6';
-const publicKey = '0d6cfd363cc74d7f35a76e5cb7eedefb';
-const data = timestamp + privateKey + publicKey;
-const hash = crypto.createHash('md5').update(data).digest("hex");
-
-const api = `http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
-
-//=====================
-
-let schema = buildSchema(`
-  type Query {
-    characters: String,
-    character: String
-  }
-`);
-
-// Root provides a resolver function for each API endpoint
-let root = {
-  postTitle: () => {
-    return 'Build a Simple GraphQL Server With Express and NodeJS';
-  },
-  blogTitle: () => {
-    return 'scotch.io';
-  }
-};
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: () => ({
+    allCharacters: {
+      type: new GraphQLList(CharactersType),
+      resolve (root) {
+        return axios.get(api)
+          .then(response => {
+            return response.data.data.results;
+          });
+      }
+    }
+  })
+});
 
 const app = express();
 
@@ -51,9 +69,12 @@ app.get('/json', (req, res) => {
     });
 })
 
+const schema  = new GraphQLSchema({
+  query: RootQuery,
+})
+
 app.use('/graphiql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true //Set to false if you don't want graphiql enabled
 }));
 
