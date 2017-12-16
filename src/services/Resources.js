2968cbeae449dc63965454480bee6f43bf74d24f
Rafael Bernardo <rafamaxber@gmail.com>
2017-12-16 01:55:51 -0200

    add tests in resources

--- src/services/Resources.js
+++ src/services/Resources.js
@@ -1,11 +1,17 @@
 const generateMD5Hash = require('../helpers/MD5');
-/*
 const axios = require('axios');
 
 const routes = {
   characters: 'characters',
 };
-*/
+
+const urlApiParameters = {
+  BASE_URL: (process.env.NODE_ENV === 'test' ? 'http://teste.com.br' : process.env.URL_API),
+  PUBLIC_KEY: (process.env.NODE_ENV === 'test' ? '99999999999' : process.env.PUBLIC_KEY),
+  PRIVATE_KEY: (process.env.NODE_ENV === 'test' ? '888888888888' : process.env.PRIVATE_KEY),
+  TIMESTAMP: '',
+  PATH: '',
+};
 
 function generateApiUrl({
   BASE_URL, TIMESTAMP, PATH, PUBLIC_KEY, PRIVATE_KEY,
@@ -17,48 +23,16 @@ function generateApiUrl({
   return `${BASE_URL}${PATH}?ts=${TIMESTAMP}&apikey=${PUBLIC_KEY}&hash=${hash}`;
 }
 
-/*
-const timestamp = Date.now();
-const privateKey = process.env.PRIVATE_KEY;
-const publicKey = process.env.PUBLIC_KEY;
-const API_URL = process.env.URL_API;
-const hash = generateMD5Hash(timestamp + privateKey + publicKey);
-const requiredParams = `?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
-
-const getAPI_URL = (path, args = {}) => {
-  return `${API_URL}${path}${requiredParams}${args}`;
-};
-
-
-const instance = axios.create({
-  baseURL: process.env.URL_API,
-});
-
-class Resources {
-  constructor() {
-    this.timestamp = Date.now();
-    this.privateKey = process.env.PRIVATE_KEY;
-    this.publicKey = process.env.PUBLIC_KEY;
-    this.API_URL = process.env.URL_API;
-    this.hash = crypto
-      .createHash('md5')
-      .update(this.timestamp + this.privateKey + this.publicKey)
-      .digest('hex');
-    this.requiredParams = `?ts=${this.timestamp}&apikey=${this.publicKey}&hash=${this.hash}`;
-  }
-
-  buildApiUrl(path, args = {}) {
-    return `${this.API_URL}${this.path}${this.requiredParams}${args}`;
-  }
-
-  httpGetAllCharacters() {
-    return axios.get(this.buildApiUrl(routes.characters))
-      .then(res => res.data)
-      .catch(error => Error(error));
-  }
+function httpGetAllCharacters() {
+  const parameters = Object.assign({}, urlApiParameters);
+  parameters.TIMESTAMP = Date.now();
+  parameters.PATH = routes.characters;
+  return axios.get(generateApiUrl({ ...parameters }));
 }
-*/
 
 module.exports = {
   generateApiUrl,
+  httpGetAllCharacters,
+  routes,
+  urlApiParameters,
 };
