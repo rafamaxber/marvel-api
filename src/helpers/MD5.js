4aaaa2bce4a4032e58a866e6771a765f535cabf0
Rafael Bernardo <rafamaxber@gmail.com>
2017-12-16 03:14:33 -0200

    add throw to md5 helper

--- src/helpers/MD5.js
+++ src/helpers/MD5.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto');
 
 function generateMD5Hash(value) {
+  if (!value) throw Error('Is necerrary send the parameter!');
   return crypto.createHash('md5').update(String(value)).digest('hex');
 }
 
--- src/helpers/MD5.test.js
+++ src/helpers/MD5.test.js
@@ -9,5 +9,11 @@ describe('MD5', () => {
     test('Should return a valid MD5', () => {
       expect(generateMD5Hash('gera teste')).toBe('4cf5c44e77fa25902134e7cc68c8f441');
     });
+
+    test('Should return error when parameter is invalid', () => {
+      expect(() => {
+        generateMD5Hash();
+      }).toThrow();
+    });
   });
 });
