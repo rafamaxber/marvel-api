3c85b4ac548238e0ca326f6d55344c0c7ff8d56d
Rafael Bernardo <rafamaxber@gmail.com>
2018-06-23 23:07:42 -0300

    add babel and many schemas

--- src/modules/Application.js
+++ src/modules/Application.js
@@ -2,7 +2,6 @@ import path from 'path'
 import { buildSchema } from 'graphql'
 import { mergeResolvers, mergeTypes, fileLoader } from 'merge-graphql-schemas'
 import graphqlHTTP from 'express-graphql'
-import { schemaCharacters, resolverCharacters } from './Characters'
 
 const typesArray = fileLoader(path.join(__dirname, '/**/*.graphql'))
 const resolversArray = fileLoader(path.join(__dirname, '/**/resolvers/*.js'))
--- src/modules/Characters/index.js
+++
@@ -1,6 +0,0 @@
-import { importSchema } from 'graphql-import'
-
-export const schemaCharacters = importSchema(
-  `${__dirname}/schemas/characters.graphql`
-)
-export { resolverCharacters } from './resolvers'
